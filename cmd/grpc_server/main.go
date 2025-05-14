792e83c1b22f8a2f986639a56c8883c0162fa980
Igor Popov <spartak-992010@yandex.ru>
2025-05-14 18:54:04 +0600

    add grpc methods

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
+	"github.com/brianvoe/gofakeit"
 	desc "github.com/igorezka/chat-server/pkg/chat_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 const grpcPort = 50052
 
 type server struct {
 	desc.UnimplementedChatV1Server
+}
+
+func (*server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("CreateRequest: %v", req)
+
+	return &desc.CreateResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+func (*server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("DeleteRequest: %v", req)
+
+	return &emptypb.Empty{}, nil
+}
+
+func (*server) SendMessage(_ context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("SendMessage: %s", req.GetText())
+
+	return &emptypb.Empty{}, nil
 }
 
 func main() {
